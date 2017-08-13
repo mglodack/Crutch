c90be8d3e118b401f031b5c0898ecd8489c9ea5d
Mathew Glodack <mathewg@procore.com>
2017-08-13 10:14:53 -0700

    Misc code clean up
    * Remove unnecessary comments
    * Update vsix manifest

--- src/Crutch/InvalidPropertyAssignmentAnalyzer.cs
+++ src/Crutch/InvalidPropertyAssignmentAnalyzer.cs
@@ -6,8 +6,6 @@ namespace Crutch
 {
     class InvalidPropertyAssignmentAnalyzer
     {
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         static readonly LocalizableString _Title = new LocalizableResourceString(
             nameOfLocalizableResource: nameof(Resources.InvalidPropertyAssignmentTitle),
             resourceManager: Resources.ResourceManager,
--- src/Crutch/UnusedParametersAnalyzer.cs
+++ src/Crutch/UnusedParametersAnalyzer.cs
@@ -10,8 +10,6 @@ namespace Crutch
 {
     class UnusedParametersAnalyzer
     {
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         static readonly LocalizableString _Title = new LocalizableResourceString(
             nameOfLocalizableResource: nameof(Resources.UnusedParameterTitle),
             resourceManager: Resources.ResourceManager,
